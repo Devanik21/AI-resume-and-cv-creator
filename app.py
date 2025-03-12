a0155a9a6edfeee7482566ec5d7634a4f16acdce
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-03-12 21:02:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown(
             box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
         }
         .custom-card {
-            background-color: white;
+            background-color: black;
             border-radius: 10px;
             padding: 20px;
             box-shadow: 0 4px 8px rgba(0,0,0,0.1);
